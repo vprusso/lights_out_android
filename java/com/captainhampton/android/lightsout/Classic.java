134e9143bf3b4826a9746f1df4f386e984e9f167
Vincent Russo <vincentrusso1@gmail.com>
2016-01-29 19:23:48 -0500

    adding to lights out

---
+++ java/com/captainhampton/android/lightsout/Classic.java
@@ -0,0 +1,19 @@
+package com.captainhampton.android.lightsout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Classic extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.classic);
+        setupVariables();
+    }
+
+    private void setupVariables() {
+
+    }
+
+}
---
+++ java/com/captainhampton/android/lightsout/HowToPlay.java
@@ -0,0 +1,13 @@
+package com.captainhampton.android.lightsout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class HowToPlay extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.how_to_play);
+    }
+}
