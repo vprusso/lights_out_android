5681fc3767176470e413df93ab3fe597bd193e3d
Vincent Russo <vincentrusso1@gmail.com>
2016-02-05 16:45:05 -0500

    refactoring the main light board to be treated as a grid

--- java/com/captainhampton/android/lightsout/Classic.java
+++ java/com/captainhampton/android/lightsout/Classic.java
@@ -12,28 +12,29 @@ public class Classic extends AppCompatActivity implements View.OnClickListener {
     public Button bHome, bHint, bReset;
     public TextView tvNumMoves, tvLevelTime;
 
-    public static final int[][] LEVELS = {
-            {7,11,12,13,17},
-            {5,9,10,11,13,14,15,19},
-            {3,4,7,9,11,12,13,15,17,20,21},
-            {0,1,3,4,5,9,15,19,20,21,23,24},
-            {0,1,3,4,5,7,9,11,12,13,15,17,19,20,21,23,24},
-            {10,12,14},
-            {0,2,4,5,7,9,15,17,19,20,22,24}
+    private static final int[][][] LEVELS = {
+            { {1,2}, {2,1}, {2,2}, {2,3}, {3,2} },
+            { {1,0}, {1,4}, {2,0}, {2,1}, {2,3}, {2,4}, {3,0}, {3,4} },
+            { {0,3}, {0,4}, {1,2}, {1,4}, {2,1}, {2,2}, {2,3}, {3,0}, {3,2}, {4,0}, {4,1}  },
+            { {0,0}, {0,1}, {0,3}, {0,4}, {1,0}, {1,4}, {3,0}, {3,4}, {4,0}, {4,1}, {4,3}, {4,4} },
+            { {0,0}, {0,1}, {0,3}, {0,4}, {1,0}, {1,2}, {1,4}, {2,1}, {2,2}, {2,3}, {3,0}, {3,2}, {3,4}, {4,0}, {4,1}, {4,3}, {4,4} },
+            { {2,0}, {2,2}, {2,4} },
+            { {0,0}, {0,2}, {0,4}, {1,0}, {1,2}, {1,4}, {3,0}, {3,2}, {3,4}, {4,0}, {4,2}, {4,4} },
     };
 
-    private static final int[] BUTTON_IDS = {
-      R.id.button0, R.id.button1, R.id.button2, R.id.button3, R.id.button4,
-      R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9,
-      R.id.button10, R.id.button11, R.id.button12, R.id.button13, R.id.button14,
-      R.id.button15, R.id.button16, R.id.button17, R.id.button18, R.id.button19,
-      R.id.button20, R.id.button21, R.id.button22, R.id.button23, R.id.button24,
+    private static final int[][] LIGHT_IDS = {
+            { R.id.button0, R.id.button1, R.id.button2, R.id.button3, R.id.button4 },
+            { R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9 },
+            { R.id.button10, R.id.button11, R.id.button12, R.id.button13, R.id.button14 },
+            { R.id.button15, R.id.button16, R.id.button17, R.id.button18, R.id.button19 },
+            { R.id.button20, R.id.button21, R.id.button22, R.id.button23, R.id.button24 },
     };
 
-    private static final int row_col_length = 5;
+    private static final int NUM_ROWS = 5;
+    private static final int NUM_COLS = 5;
 
-    private Button[] buttons = new Button[BUTTON_IDS.length];
-    private boolean[] button_states = new boolean[BUTTON_IDS.length];
+    private Button[][] lights = new Button[NUM_ROWS][NUM_COLS];
+    private boolean[][] light_states = new boolean[NUM_ROWS][NUM_COLS];
 
     private int level_num = 0;
     private int num_moves;
@@ -61,73 +62,61 @@ public class Classic extends AppCompatActivity implements View.OnClickListener {
         tvNumMoves.setText(String.format("%d", num_moves));
     }
 
-    private void activateButton(Button b, int button_id) {
-        button_states[button_id] = Boolean.TRUE;
-        buttons[button_id].setBackgroundColor(Color.RED);
+    private void activateButton(int x, int y) {
+        light_states[x][y] = Boolean.TRUE;
+        lights[x][y].setBackgroundColor(Color.RED);
     }
 
-    private void deactivateButton(Button b, int button_id) {
-        button_states[button_id] = Boolean.FALSE;
-        buttons[button_id].setBackgroundColor(Color.WHITE);
+    private void deactivateButton(int x, int y) {
+        light_states[x][y] = Boolean.FALSE;
+        lights[x][y].setBackgroundColor(Color.WHITE);
     }
 
-    private boolean isOffRightScreen(int button_id) {
-        return ( (button_id + 1) % row_col_length == 0 );
-    }
-
-    private boolean isOffLeftScreen(int button_id) {
-        return ( button_id % row_col_length == 0 );
-    }
-
-    private boolean isButtonActive(int button_id) {
-        return ( button_states[button_id] == Boolean.TRUE );
-    }
-
-    private void setButtonRadius(Button[] buttons, int button_id) {
-
-        int top = button_id - row_col_length;
-        int bottom = button_id + row_col_length;
-        int left = button_id - 1;
-        int right = button_id + 1;
-
-        if ( isButtonActive( button_id )  )
-            deactivateButton( buttons[button_id], button_id );
+    private void flipLight(int x, int y) {
+        if (isLightActive(x, y))
+            deactivateButton(x,y);
         else
-            activateButton(buttons[button_id], button_id);
-
-        if (top >= 0 && top < BUTTON_IDS.length) {
-            if ( isButtonActive( top ) )
-                deactivateButton( buttons[top], top );
-            else
-                activateButton( buttons[top], top );
-        }
-
-        if (bottom >= 0 && bottom < BUTTON_IDS.length) {
-            if ( isButtonActive( bottom ) )
-                deactivateButton( buttons[bottom], bottom );
-            else
-                activateButton( buttons[bottom], bottom );
-        }
-
-        if (left >= 0 && left < BUTTON_IDS.length && !isOffLeftScreen(button_id) ) {
-            if ( isButtonActive( left ) )
-                deactivateButton( buttons[left], left );
-            else
-                activateButton( buttons[left], left );
-        }
-
-        if (right >= 0 && right < BUTTON_IDS.length && !isOffRightScreen(button_id) ) {
-            if ( isButtonActive( right ) )
-                    deactivateButton( buttons[right], right );
-            else
-                activateButton( buttons[right], right );
-        }
+            activateButton(x, y);
     }
 
-    private void initBoard() {
-        for (int i = 0; i < BUTTON_IDS.length; i++) {
-            buttons[i].setBackgroundColor(Color.WHITE);
-            button_states[i] = Boolean.FALSE;
+    private boolean isLightOutOfBounds(int x, int y) {
+        return ( x >= NUM_ROWS || x < 0 || y >= NUM_COLS || y < 0 );
+    }
+
+    private boolean isLightActive(int x, int y) {
+        return ( light_states[x][y] == Boolean.TRUE );
+    }
+
+    private void pressedLights(int x, int y) {
+        int top = x - 1;
+        int bot = x + 1;
+        int left = y - 1;
+        int right = y + 1;
+
+        flipLight(x,y);
+
+        if (!isLightOutOfBounds(top, y))
+            flipLight(top, y);
+
+
+        if (!isLightOutOfBounds(bot, y))
+            flipLight(bot, y);
+
+
+        if (!isLightOutOfBounds(x, left))
+            flipLight(x, left);
+
+
+        if (!isLightOutOfBounds(x, right))
+            flipLight(x, right);
+
+    }
+
+    private void clearBoard() {
+        for (int i = 0; i < NUM_ROWS; i++) {
+            for (int j = 0; j < NUM_COLS; j++) {
+                deactivateButton(i, j);
+            }
         }
         resetTimer();
         resetNumMoves();
@@ -135,62 +124,70 @@ public class Classic extends AppCompatActivity implements View.OnClickListener {
 
     private void setupBoard() {
 
-        initBoard();
+        clearBoard();
 
-        for (int i = 0; i < LEVELS[level_num].length; i++ ) {
-            buttons[ LEVELS[level_num][i] ].setBackgroundColor(Color.RED);
-            button_states[ LEVELS[level_num][i] ] = Boolean.TRUE;
+        for (int i = 0; i < LEVELS[level_num].length; i++) {
+            int x = LEVELS[level_num][i][0];
+            int y = LEVELS[level_num][i][1];
+
+            activateButton(x, y);
         }
     }
 
-    private boolean checkVictory(Button[] buttons) {
-        for (int i = 0; i < BUTTON_IDS.length; i++) {
-            if (isButtonActive(i))
-                return false;
+    private boolean checkVictory() {
+        for (int i = 0; i < NUM_ROWS; i++) {
+            for (int j = 0; j < NUM_COLS; j++) {
+                if (isLightActive(i, j)) {
+                    return false;
+                }
+            }
         }
         return true;
     }
 
     private void setupVariables() {
 
-        bHome = (Button)findViewById(R.id.bHome);
+        bHome = (Button) findViewById(R.id.bHome);
         bHome.setOnClickListener(this);
 
-        bReset = (Button)findViewById(R.id.bReset);
+        bReset = (Button) findViewById(R.id.bReset);
         bReset.setOnClickListener(this);
 
-        tvNumMoves = (TextView)findViewById(R.id.tvNumMoves);
-        tvLevelTime = (TextView)findViewById(R.id.tvLevelTime);
+        tvNumMoves = (TextView) findViewById(R.id.tvNumMoves);
+        tvLevelTime = (TextView) findViewById(R.id.tvLevelTime);
 
-        for (int i = 0; i < BUTTON_IDS.length; i++) {
-            final int button_id = i;
-            buttons[button_id] = (Button)findViewById(BUTTON_IDS[button_id]);
-            buttons[button_id].setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
+        for (int i = 0; i < NUM_ROWS; i++) {
+            for (int j = 0; j < NUM_COLS; j++) {
 
-                    if (buttons[button_id].isPressed()) {
-                        setButtonRadius(buttons, button_id);
-                        num_moves++;
-                        // Put in timer stuff here
-                        tvNumMoves.setText(String.format("%d", num_moves));
-                        tvLevelTime.setText(String.format("%d", level_time));
+                final int x = i;
+                final int y = j;
+                lights[x][y] = (Button)findViewById(LIGHT_IDS[x][y]);
+                lights[x][y].setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        if (lights[x][y].isPressed()) {
+                            pressedLights(x, y);
+                            num_moves++;
+                            tvNumMoves.setText(String.format("%d", num_moves));
+                            tvLevelTime.setText(String.format("%d", level_time));
+                        }
+
+                        if ( checkVictory() ) {
+                            // TODO : victory dance
+                            level_num++;
+                            if (level_num <= LEVELS.length) {
+                                setLevel(level_num);
+                                setupBoard();
+                            } else {
+                                // TODO
+                            }
+                        } // end checkVictory
 
                     }
+                });
 
-                    if ( checkVictory(buttons) ) {
-                        // TODO : victory dance
-                        level_num++;
-                        if (level_num <= LEVELS.length) {
-                            setLevel(level_num);
-                            setupBoard();
-                        } else {
-                            // TODO
-                        }
-                    } // end checkVictory
-
-                }
-            });
+            }
         }
 
         setupBoard();
